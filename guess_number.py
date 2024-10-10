35c3ecf7d0eca1aa3f625ae43434f7bba75c6b41
Gleb Repin <gmrepin@mail.ru>
2024-10-10 14:18:35 +0300

    fix syntax

--- guess_number.py
+++ guess_number.py
@@ -1,14 +1,14 @@
 from random import randint
 
 
-number = randint(1,100)
+number = randint(1, 100)
 print('Угадайте число от 1 до 100')
 
 while True:
     guess = int(input('Введите число: '))
     if guess > number:
         print('Ваше число больше загаданого')
-    elif guess <number:
+    elif guess < number:
         print('Ваше число меньше загаданого')
     elif guess == number:
         break
